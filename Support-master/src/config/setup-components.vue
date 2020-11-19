dca335a70c59af1d3546dce2d411395fdc2cd061
gilangtungul <gilang.wicaksana@students.amikom.ac.id>
2020-11-19 23:54:52 +0700

    first commit'

---
+++ Support-master/src/config/setup-components.vue
@@ -0,0 +1,15 @@
+<!-- // Core Components -->
+
+
+import Widget from '../views/Widget.vue';
+
+
+function setupComponents(Vue){
+
+  Vue.component('widget', Widget);
+}
+
+
+export {
+  setupComponents
+}
